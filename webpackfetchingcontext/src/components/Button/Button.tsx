f658f72966c1573dcfef3eabd61656788a1f5004
Olivka07 <uchenik.ikt@yandex.ru>
2024-12-21 14:05:45 +0300

    added fetching app with context

---
+++ webpackfetchingcontext/src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Button as ButtonAnt, ConfigProvider} from 'antd'
+import { useAppContext } from '../../context/AppContext/AppContext';
+
+const Button = () => {
+    const {fetchPhotos} = useAppContext()
+
+    return (
+        <ButtonAnt style={{marginBottom: '20px'}} onClick={fetchPhotos}>
+            Получить фото
+        </ButtonAnt>
+    );
+};
+    // return (
+    //     <ConfigProvider
+    //         theme={{
+    //             components: {
+    //                 Button: {
+    //                     defaultBg: 'rgb(255, 255, 54)',
+    //                     defaultColor: 'black',
+    //                     fontWeight: '600',
+    //                     colorPrimaryHover: 'black',
+    //                 }
+    //             },
+    //             token: {
+    //             }
+    //         }}
+    //     >
+    //         <ButtonAnt style={{marginBottom: '20px'}} onClick={fetchPhotos}>
+    //             Получить фото
+    //         </ButtonAnt>
+    //     </ConfigProvider>
+    // );
+// };
+
+export default Button;
