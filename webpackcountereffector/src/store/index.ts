07da5e18b3dd83aad2c95245547796fbf39be1a3
Olivka07 <uchenik.ikt@yandex.ru>
2025-01-02 10:03:33 +0300

    added eslint && prettier && workflow rules

--- webpackcountereffector/src/store/index.ts
+++ webpackcountereffector/src/store/index.ts
@@ -1,8 +1,5 @@
 import { createEvent, createStore } from "effector";
 
-export const changeCounter = createEvent<number>('Change Counter')
+export const changeCounter = createEvent<number>("Change Counter");
 
-export const $counter = createStore(0)
-    .on(changeCounter, (state, payload) => state + payload)
-
-    
+export const $counter = createStore(0).on(changeCounter, (state, payload) => state + payload);
