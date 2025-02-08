1934c03f6802d850d1d27da0a6573a8536609ec2
Olivka07 <uchenik.ikt@yandex.ru>
2025-02-08 15:29:38 +0300

    added accessibility

--- webpackcountercontext/src/App.test.tsx
+++ webpackcountercontext/src/App.test.tsx
@@ -1,5 +1,29 @@
+import App from "./App";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CounterProvider from "./context/CounterContext";
+
 describe("Component: App", () => {
   it("should render correctly", () => {
-    expect("a").toBe("a");
+    const expectTestId = "counter";
+
+    render(<App />);
+
+    expect(screen.getByTestId(expectTestId)).toBeInTheDocument();
+  });
+
+  it("should equal counter 1 after clicking on the Inc Button", async () => {
+    const counterTestId = "counter";
+    const incButtonTestId = "incButton";
+
+    render(
+      <CounterProvider>
+        <App />
+      </CounterProvider>
+    );
+
+    await userEvent.click(screen.getByTestId(incButtonTestId));
+
+    expect(screen.getByTestId(counterTestId)).toHaveTextContent("1");
   });
 });
