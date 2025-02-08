4e4249f4c4ea42b288130f4544ec95ab626c4c9f
Olivka07 <uchenik.ikt@yandex.ru>
2025-02-08 17:02:40 +0300

    fixed ci

--- webpackcounterredux/src/App.tsx
+++ webpackcounterredux/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
       <div>
         <h1 style={{ textAlign: "center" }}>Counter:{counter}</h1>
-        <div style={{ display: "flex", justifyContent: "space-between"}}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           <button style={{ padding: "5px" }} onClick={() => dispatch(changeCounter(1))}>
             Increment
           </button>
